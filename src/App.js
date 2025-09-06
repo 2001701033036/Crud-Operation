d857e496ddc5067fd3b6c9a3a7c5897f9f6a4c4d
Samuel <sam324240@gmail.com>
2025-09-06 18:13:12 +0530

    figma only

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import './App.css';
-import Create from './Componenets/Create';
+// import Create from './Componenets/Create';
 import Figma from './Componenets/Figma';
 import { Routes, Route } from 'react-router-dom';
 
@@ -16,8 +16,8 @@ function App() {
 
       {/* Routing */}
       <Routes>
-        <Route path="/" element={<Create />} />
-        <Route path="/figma" element={<Figma />} />
+        <Route path="/" element={<Figma />} />
+        {/* <Route path="/figma" element={<Figma />} /> */}
         {/* Optional: 404 Not Found route */}
         <Route path="*" element={<h2>404 Page Not Found</h2>} />
       </Routes>
